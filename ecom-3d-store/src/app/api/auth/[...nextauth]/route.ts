2ec18712c94df109a01c4dab532050ab71602c7a
Dhruv Shah <dhruv10.shah@gmail.comm>
2025-04-14 02:18:43 +0530

    Changes made for vercel build

--- ecom-3d-store/src/app/api/auth/[...nextauth]/route.ts
+++ ecom-3d-store/src/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,5 @@
-// app/api/auth/[...nextauth]/route.ts
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    strategy: "database",
-  },
-  pages: {
-    signIn: '/auth/signin',
-  },
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
